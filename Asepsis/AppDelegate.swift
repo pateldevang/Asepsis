f66db62c731d2c8f2ed6faf1b537ebc356764faa
pateldevang <devangpatel92@gmail.com>
2019-09-21 23:53:19 +0530

    increase sleep time

--- Asepsis/AppDelegate.swift
+++ Asepsis/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        // Increse Launch Time
+        Thread.sleep(forTimeInterval: 1.3)
         return true
     }
 
