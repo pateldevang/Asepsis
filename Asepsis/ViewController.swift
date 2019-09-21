6dc71889cbd1da510918b46a7f9192a177691723
pateldevang <devangpatel92@gmail.com>
2019-09-21 21:25:39 +0530

    auth done

--- Asepsis/ViewController.swift
+++ Asepsis/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     //MARK: - Outlets
     @IBOutlet weak var empID: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var load: UIActivityIndicatorView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.load.isHidden = true
     }
     
     
-
+    @IBAction func signIn(_ sender: UIButton) {
+        self.load.isHidden = false
+        load.startAnimating()
+        Auth.auth().signIn(withEmail: empID.text!, password: password.text!, completion: { user, error in
+            
+            if let firebaseError = error {
+                self.load.isHidden = true
+                print(firebaseError.localizedDescription)
+                let alert = UIAlertController(title: "Authentication failed", message: "Invalid password please try again", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true)
+                return
+            }
+            let homeView = self.storyboard?.instantiateViewController(withIdentifier: "homeViewController") as! homeViewController
+            self.present(homeView, animated: true, completion: nil)
+            print("Success!")
+            
+        })
+    }
+    
 
 }
 
---
+++ Asepsis/homeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  homeViewController.swift
+//  Asepsis
+//
+//  Created by Devang Patel on 21/09/19.
+//  Copyright © 2019 Devang Patel. All rights reserved.
+//
+
+import UIKit
+
+class homeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
